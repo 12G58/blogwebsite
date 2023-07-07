fb3a7d3143141a88a9846812191d6e8d32cfa870
unknown <sasha.kuhu@gmail.com>
2023-07-07 22:36:31 +0530

    First commit

---
+++ Desktop/3d_portfolio/src/components/index.js
@@ -0,0 +1,24 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Logic from './Logic';
+import Tech from './Tech';
+import Science from './Science';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  Logic,
+  Tech,
+  Science,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
